6479877fae9c9630a6f9ca79c8659e779388f7e7
Hans <hansmandersingh1998@gmail.com>
2020-05-14 13:42:38 -0500

    Created to cb functions to decrease size of css and img file

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const imageMin = require('gulp-imagemin');
+
+function smallCss(cb) {
+  gulp.src('src/css/*')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dest/css'))
+  cb();
+}
+
+function imageSmall(cb) {
+  gulp.src('src/img/*')
+    .pipe(imageMin())
+    .pipe(gulp.dest('dest/img'))
+  cb();
+}
+
